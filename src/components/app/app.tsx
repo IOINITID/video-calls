278b044eb98e3a30bc31e2581b2e8b137d0dfdee
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-09-06 22:55:00 +0300

    Updated app component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,21 @@
+import { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import Peer from 'simple-peer';
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:8080');
+
 const App = () => {
+  const [me, setMe] = useState('');
+  const [stream, setStream] = useState();
+  const [receivingCall, setReceivingCall] = useState(false);
+  const [caller, setCaller] = useState('');
+  const [callerSignal, setCallerSignal] = useState();
+  const [callAcepted, setCallAcepted] = useState(false);
+  const [idToCall, setIdToCall] = useState('');
+  const [callEnded, setCallEnded] = useState(false);
+  const [name, setName] = useState('');
+
   return <div></div>;
 };
 
