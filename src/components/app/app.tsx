9a731ea036a55939d074a7ea398b98a8fe351867
IOINITID <50173108+IOINITID@users.noreply.github.com>
2021-09-07 13:23:44 +0300

    Updated socket url

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -5,7 +5,7 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import Peer from 'simple-peer';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:8080');
+const socket = io('https://ioinitid-video-calls-server.herokuapp.com:8080');
 
 const App = () => {
   const [me, setMe] = useState('');
