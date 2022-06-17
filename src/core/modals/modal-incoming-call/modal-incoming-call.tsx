aeca212f4aae22f1fcb973b7cfc4754e15584fb6
IOINITID <50173108+IOINITID@users.noreply.github.com>
2022-06-18 00:31:03 +0300

    Updated user call modal

--- src/core/modals/modal-incoming-call/modal-incoming-call.tsx
+++ src/core/modals/modal-incoming-call/modal-incoming-call.tsx
@@ -1,4 +1,4 @@
-import { Modal, Typography } from '@mui/material';
+import { Box, Modal, Typography } from '@mui/material';
 import { Button } from 'core/components/button';
 import { socket } from 'core/utils/socket';
 import { User } from 'modules/user/services/types';
@@ -16,36 +16,59 @@ const ModalIncomingCall = ({ open, onClose, user }: ModalIncomingCallProps) => {
 
   return (
     <Modal open={open} onClose={onClose}>
-      <>
+      <Box
+        sx={{
+          display: 'grid',
+          rowGap: '32px',
+          alignItems: 'center',
+          background: '#ffffff',
+          borderRadius: '8px',
+          padding: '16px 32px',
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      >
         <Typography>Вам звонит пользователь: {user?.name ? user.name : 'Неизвестный'}</Typography>
 
-        <Button
-          variant="contained"
-          color="success"
-          onClick={() => {
-            if (user?.id) {
-              navigate(`meet/${user.id}`);
-            }
-
-            onClose();
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(2, max-content)',
+            alignItems: 'center',
+            justifyContent: 'center',
+            columnGap: '16px',
           }}
         >
-          Ответить
-        </Button>
-        <Button
-          variant="contained"
-          color="error"
-          onClick={() => {
-            if (user?.id) {
-              socket.emit('client:meet_end_call', user.id);
-            }
+          <Button
+            variant="contained"
+            color="success"
+            onClick={() => {
+              if (user?.id) {
+                navigate(`meet/${user.id}`);
+              }
 
-            onClose();
-          }}
-        >
-          Отклонить
-        </Button>
-      </>
+              onClose();
+            }}
+          >
+            Ответить
+          </Button>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={() => {
+              if (user?.id) {
+                socket.emit('client:meet_end_call', user.id);
+              }
+
+              onClose();
+            }}
+          >
+            Отклонить
+          </Button>
+        </Box>
+      </Box>
     </Modal>
   );
 };
