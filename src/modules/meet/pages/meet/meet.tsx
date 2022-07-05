c2eee14e54017c87784d02d79853af26cc137aa5
IOINITID <50173108+IOINITID@users.noreply.github.com>
2022-07-05 22:36:23 +0300

    Updated ice servers

--- src/modules/meet/pages/meet/meet.tsx
+++ src/modules/meet/pages/meet/meet.tsx
@@ -129,7 +129,26 @@ const Meet = () => {
     if (stream) {
       // NOTE: Создание peer соединения
       peerConnection.current = new RTCPeerConnection({
-        iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+        iceServers: [
+          {
+            urls: 'stun:openrelay.metered.ca:80',
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject',
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject',
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+            username: 'openrelayproject',
+            credential: 'openrelayproject',
+          },
+        ],
       });
 
       // NOTE: Добавление обработчика события icecandidate
