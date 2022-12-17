7149c08e56036d157650912cd01cbdf4f8d2f6f4
IOINITID <50173108+IOINITID@users.noreply.github.com>
2022-12-17 17:12:18 +0300

    Updated toast styles

--- src/core/components/root/root.tsx
+++ src/core/components/root/root.tsx
@@ -1,16 +1,15 @@
+import { Provider } from 'react-redux';
 import { ThemeProvider } from '@mui/material';
+import { HashRouter } from 'react-router-dom';
 import { store } from 'core/store';
 import { globalStyles } from 'core/styles';
 import { theme } from 'core/theme';
-import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { App } from 'core/components/app';
 import { ToastContainer } from 'react-toastify';
-import { injectStyle } from 'react-toastify/dist/inject-style';
-import { App } from '../app';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Root = () => {
   globalStyles();
-  injectStyle();
 
   return (
     <Provider store={store}>
